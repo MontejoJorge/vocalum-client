da45c160dceb37d4d879ede7a7fbd00f1ce59c7c
MontejoJorge <jorgemon.lopez@gmail.com>
2022-10-11 13:27:20 +0200

    navBar component

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/profile">Profile</RouterLink>
-        <RouterLink to="/create">Create</RouterLink>
-      </nav>
-    </div>
+    <NavBar/>
   </header>
 
   <RouterView />
