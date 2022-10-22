dd5c4a0b3e30f83a3a3b3b72777a4779b5cd7fbf
MontejoJorge <jorgemon.lopez@gmail.com>
2022-10-22 12:43:35 +0200

    photo from storage folder

--- src/util/photo.js
+++ src/util/photo.js
@@ -1,7 +1,7 @@
-const url = import.meta.env.VITE_API_URL;
+const url = import.meta.env.VITE_STORAGE_URL;
 
 const getPhotoURL = (photoUUID) => {
-  return `${url}/ads/${photoUUID}/photo`;
+  return `${url}/${photoUUID}.jpg`;
 };
 
 export default getPhotoURL;
